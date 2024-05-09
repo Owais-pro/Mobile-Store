8f1b59fb0ba55634191228507aa7b663bd9b08e9
Rao-Owais <rao.owais2020@gmail.com>
2024-05-09 17:01:25 +0500

    updated

--- mobile.js
+++ mobile.js
@@ -69,7 +69,7 @@ const phones = [
 const cartArr = [];
 const div = document.querySelector('#display');
 const message = document.querySelector('#message');
-const cartItem = document.querySelector('.cart-item');
+const cartItem = document.querySelector('#cart-item');
 for(let i=0; i<phones.length; i++)
     {
         div.innerHTML += `
@@ -98,24 +98,27 @@ for(let i=0; i<phones.length; i++)
             }  
            
             message.style.display = 'block';
+            console.log(cartArr[index])
 
     };
-   function displayCart(){
-    for(let i=0; i<cartArr.length; i++){
+
+    for(let j=0; j<cartArr.length; j++){
         cartItem.innerHTML += `
         <div class="card" style="width: 18rem;">
   <img src="..." class="card-img-top" alt="...">
   <div class="card-body">
-    <h4 class="card-title">${cartArr[i].brand + ' ' + cartArr[i].model}</h4>
-    <h6 class="card-title">RAM: ${cartArr[i].ram}</h6>
-    <h6 class="card-title">ROM: ${cartArr[i].rom}</h6>
-    <h5 class="card-title">Price: ${cartArr[i].price}</h5>
-    <a href="#" onclick="AddCart(${i})" class="btn btn-primary">Add To Cart</a>
+    <h4 class="card-title">${cartArr[j].brand + ' ' + cartArr[j].model}</h4>
+    <h6 class="card-title">RAM: ${cartArr[j].ram}</h6>
+    <h6 class="card-title">ROM: ${cartArr[j].rom}</h6>
+    <h5 class="card-title">Price: ${cartArr[j].price}</h5>
+    <a href="#" onclick="AddCart(${j})" class="btn btn-primary">Add To Cart</a>
   </div>
 </div>
         `
     }
 
-   }
+   function displayCart(){
+    window.location = 'cart.html';
+   };
     
     
